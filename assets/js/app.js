aea78942091413bb5a6ddde48187b8a929d3afd3
Camila Castro <ccastrorivera@uc.cl>
2022-10-03 16:54:44 -0300

    landing page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 $(function () {
-  $('[data-bs-toggle="tooltip"]').tooltip();
+  $('[data-bs-toggle="tooltip]').tooltip();
+});
+
+$(document).scroll(function () {
+  const y = $("html").scrollTop();
+
+  y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
 });
